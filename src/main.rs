94a464897dea32ef48890eff879af3e66110f29e
한진희 <0200134hjh@gmail.com>
2025-12-02 12:19:15 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,17 @@ mod engine;
 
 use axum::{Router, routing::get};
 use controller::hello_controller::hello_handler;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/hello", get(hello_handler));
 
-    println!("Rust-Autumn Server running on http://localhost:8080");
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
+    println!("Rust-Autumn Server running on http://{}", addr);
+
+    // Axum 0.7: hyper::Server 사용
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
