a935730659ab3390995c026ea8657c8ea72803d4
한진희 <0200134hjh@gmail.com>
2025-12-02 12:29:28 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ async fn main() {
     let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
     println!("Rust-Autumn Server running on http://{}", addr);
 
-    // Axum 0.7: hyper::Server 사용
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
